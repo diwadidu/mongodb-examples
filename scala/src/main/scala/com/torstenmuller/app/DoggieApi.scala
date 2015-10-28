68dcbc7d254de62f528ba717c32bd82eff5e6524
Torsten Muller <torsten@torsten-muller.com>
2015-10-27 19:37:54 -0500

    Removed Dog class and replaced it with a method to format individual dog records on output.

--- scala/src/main/scala/com/torstenmuller/app/DoggieApi.scala
+++ scala/src/main/scala/com/torstenmuller/app/DoggieApi.scala
@@ -4,8 +4,8 @@ import org.scalatra._
 import org.scalatra.json._
 import com.mongodb.casbah.Imports._
 import org.json4s.{DefaultFormats, Formats}
-//import com.torstenmuller.app.Dog
-
+import com.mongodb.casbah.commons.MongoDBObject
+import org.bson.types.ObjectId
 
 class DoggieApi(mongo: MongoCollection) extends ScalaStack with JacksonJsonSupport {
 
@@ -14,10 +14,8 @@ class DoggieApi(mongo: MongoCollection) extends ScalaStack with JacksonJsonSuppo
   override protected def renderPipeline = renderMongo orElse super.renderPipeline
 
   get("/") {
-
     for (rec <- mongo.find())
-      yield new Dog(rec)
-
+      yield this.toOutput(rec)
   }
 
 
@@ -32,6 +30,20 @@ class DoggieApi(mongo: MongoCollection) extends ScalaStack with JacksonJsonSuppo
       "[" + ls + "]"
 
   }: RenderPipeline
+
+
+
+  def toOutput(record: DBObject): DBObject = {
+
+    val id = record.as[ObjectId]("_id")
+
+    MongoDBObject(
+      "id" -> id.toString(),
+      "name" -> record.get("name"),
+      "age" -> record.as[Byte]("age"),
+      "breed" -> record.get("breed")
+    )
+  }
 
 
 }
